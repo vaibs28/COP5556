74919f02942dab4f39786b07795908c1e9777eca
Vaibhav <vsahay28@gmail.com>
2019-09-09 08:39:31 -0400

    added code for test3

--- cop5556/src/cop5556fa19/Scanner.java
+++ cop5556/src/cop5556fa19/Scanner.java
@@ -1,5 +1,4 @@
 
-
 /* *
  * Developed  for the class project in COP5556 Programming Language Principles 
  * at the University of Florida, Fall 2019.
@@ -14,9 +13,7 @@
  */
 package cop5556fa19;
 
-
 import static cop5556fa19.Token.Kind.*;
-
 
 import java.io.IOException;
 import java.io.Reader;
@@ -24,55 +21,62 @@ import java.io.Reader;
 import cop5556fa19.Token.Kind;
 
 public class Scanner {
-	
+
 	Reader r;
-	private enum State {START,HAVE_EQ,HAVE_KW}; 
+
+	private enum State {
+		START, HAVE_EQ, HAVE_KW
+	};
+
 	static int pos = -1;
 	static int line = -1;
+
 	@SuppressWarnings("serial")
-	public static class LexicalException extends Exception {	
+	public static class LexicalException extends Exception {
 		public LexicalException(String arg0) {
 			super(arg0);
 		}
 	}
-	
+
 	public Scanner(Reader r) throws IOException {
 		this.r = r;
 	}
 
-
 	public Token getNext() throws Exception {
-		    //replace this code.  Just for illustration
-		    //if (r.read() == -1) { return new Token(EOF,"eof",0,0);}
-			Token token = null;
-			State state = State.START;
-			int ch;
-			while((ch=r.read())!=-1) {
-				pos = pos + 1;
-				
-				switch(ch) {
-				case ',': token = new Token(Kind.COMMA,",",pos,line); 
-						   break;
-				case ':': int nextChar = r.read();
-						   //pos++;
-							if(nextChar==':')
-								token = new Token(Kind.COLONCOLON,"::",pos,line);
-							else 
-								token = new Token(Kind.COLON,":",pos,line);
-							break;
-				case '=': nextChar = r.read();
-						if(nextChar=='=')
-							token = new Token(Kind.REL_EQEQ,"==",pos,line);
-						else
-							token = new Token(Kind.ASSIGN,"=",pos,line);
-						break;
-					     
-				default : throw new LexicalException("Useful error message");
-				}
+		// replace this code. Just for illustration
+		// if (r.read() == -1) { return new Token(EOF,"eof",0,0);}
+		Token token = null;
+		State state = State.START;
+		int ch;
+		while ((ch = r.read()) != -1) {
+			pos = pos + 1;
+
+			switch (ch) {
+			case ',':
+				token = new Token(Kind.COMMA, ",", pos, line);
+				return token;
+			case ':':
+				int nextChar = r.read();
+				// pos++;
+				if (nextChar == ':')
+					token = new Token(Kind.COLONCOLON, "::", pos, line);
+				else
+					token = new Token(Kind.COLON, ":", pos, line);
+				return token;
+			case '=':
+				nextChar = r.read();
+				if (nextChar == '=')
+					token = new Token(Kind.REL_EQEQ, "==", pos, line);
+				else
+					token = new Token(Kind.ASSIGN, "=", pos, line);
+				return token;
+
+			default:
+				throw new LexicalException("Useful error message");
 			}
-			return token;
-			
-			
 		}
+		return token;
+
+	}
 
 }
